26f281d7d07c5778c03462973db781f163ed5e68
geajack <babybishops@gmail.com>
2019-08-03 16:11:24 +0200

    Handle unknown things gracefully

--- what_is.py
+++ what_is.py
@@ -3,13 +3,22 @@ from requests import get
 from bs4 import BeautifulSoup
 from nltk import sent_tokenize
 
+class CantFindThing(Exception):
+    
+    def __init__(self, thing):
+        self.thing = thing
+
 def fetch_wikipedia_html(title):
     URL_TEMPLATE = "https://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&redirects=&titles={title}"
     url = URL_TEMPLATE.format(title=title)
     api_response = get(url).json()
     pages = api_response["query"]["pages"]
-    page = list(pages.values())[0]
-    return page["extract"]
+    page_id = list(pages.keys())[0]
+    if page_id != "-1":
+        page = pages[page_id]
+        return page["extract"]
+    else:
+        raise CantFindThing(title)
 
 def strip_html(html):
     return BeautifulSoup(html, features="html.parser").get_text().strip()
@@ -21,4 +30,5 @@ def what_is(thing):
     return first_sentence(strip_html(fetch_wikipedia_html(thing)))
 
 if __name__ == "__main__":
-    print(what_is("charlemagne"))
+    from sys import argv
+    print(what_is(argv[1]))
--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,12 @@
 from flask import Flask
 
-from what_is import what_is
+from what_is import what_is, CantFindThing
 
 app = Flask("What Is")
+
+@app.errorhandler(CantFindThing)
+def handle_cant_find(exception):
+    return f"I don't know what \"{exception.thing}\" is.", 404
 
 @app.route("/<thing>", methods=["GET"])
 def query(thing):
